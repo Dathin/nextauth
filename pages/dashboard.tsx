d25dd343a7b46c486413bd63b9435b4dd1abf914
Pedro Caires <pedro.caires@iteris.com.br>
2021-07-11 14:11:11 -0300

    feat: can hook

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,11 +1,22 @@
 import { useContext } from "react"
 import { AuthContext } from "../contexts/AuthContext"
+import { useCan } from "../hooks/useCan"
 import { setupApiClient } from "../services/api"
 import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard(){
     const {user} = useContext(AuthContext)
-    return <h1>Dash: {user?.email}</h1>
+
+    const userCanSeeMetrics = useCan({
+        permissions: ['metrics.list']
+    });
+    
+    return (
+            <>
+            {userCanSeeMetrics && <div>Métricas</div>}
+                <h1>Dash: {user?.email}</h1>
+            </>
+        )
 }
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
